ebcb8adf2cabeb8fe641bc85aceb0a689d37781f
Tuanb2012163 <Thanhtuanb2012163@gmail.com>
2025-03-17 06:39:10 +0700

    Cập nhật code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import { MovieProvider } from './context/MovieProvider'
 
 function App() {
   const [movie, setMovie] = useState([]);
+  const [moviePopular, setMoviePopular] = useState([]);
   const [movieRate, setMovieRate] = useState([]);
+  const [movieUpcomig, setMovieUpcoming] = useState([]);
   const [movieSearch, setMovieSearch] = useState([]);
 
   const handleSearch = async (searchVal) =>{
@@ -41,19 +43,27 @@ function App() {
           Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
         }
       };
-      const url1 = 'https://api.themoviedb.org/3/movie/popular?language=viS&page=1';
-      const url2 = 'https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1';
+      const url1 = 'https://api.themoviedb.org/3/movie/now_playing?language=vi&page=1';
+      const url2 = 'https://api.themoviedb.org/3/movie/popular?language=viS&page=1';
+      const url3 = 'https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1';
+      const url4 = 'https://api.themoviedb.org/3/movie/upcoming?language=vi&page=1';
 
-      const [res1, res2] = await Promise.all([
+      const [res1, res2, res3, res4] = await Promise.all([
         fetch(url1, options),
         fetch(url2, options),
+        fetch(url3, options),
+        fetch(url4, options),
       ])
       
       const data1 = await res1.json();
       const data2 = await res2.json();
+      const data3 = await res3.json();
+      const data4 = await res4.json();
 
       setMovie(data1.results);
-      setMovieRate(data2.results);
+      setMoviePopular(data2.results);
+      setMovieRate(data3.results);
+      setMovieUpcoming(data4.results);
     };
 
     fetchMovie();
@@ -67,8 +77,10 @@ function App() {
         <Banner/>
         {movieSearch.length > 0 ? (<MovieSearch title={"Kết quả tìm kiếm"} data={movieSearch}/> ) : (
           <>
-            <MovieList title={"Phim Hot"} data={movie}/>
-            <MovieList title={"Phim Đề Cử"} data={movieRate}/>
+            <MovieList title={"Phim đang phát hành"} data={movie}/>
+            <MovieList title={"Phim thịnh hành"} data={moviePopular}/>
+            <MovieList title={"Phim có điểm số cao"} data={movieRate}/>
+            <MovieList title={"Phim sắp ra mắt"} data={movieUpcomig}/>           
           </>
         )}
 
